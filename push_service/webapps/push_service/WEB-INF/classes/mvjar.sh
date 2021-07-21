2504a39ddf8b1773fd27af0a2885f996c3887086
Jingyu He <gtouchgogo@163.com>
2021-07-21 22:09:16 +0800

    sync push_service

--- push_service/webapps/push_service/WEB-INF/classes/mvjar.sh
+++
@@ -1,4 +0,0 @@
-sudo mv /home/q/www/push_service/webapps/ROOT/WEB-INF/classes/MiPush_SDK_Server_2_2_18.jar /home/q/www/push_service/webapps/ROOT/WEB-INF/lib/
-sudo mv /home/q/www/push_service/webapps/ROOT/WEB-INF/classes/json-simple-1.1.1.jar /home/q/www/push_service/webapps/ROOT/WEB-INF/lib/
-sudo mv /home/q/www/push_service/webapps/ROOT/WEB-INF/classes/opush-server-sdk-1.0.0.jar /home/q/www/push_service/webapps/ROOT/WEB-INF/lib/
-
